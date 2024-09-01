a4e4778625a0c0032c76b5831c136ec6b7ab1a27
anu2323456 <anualias13@gmail.com>
2024-09-01 15:18:50 +0530

    addeddebug

--- app.py
+++ app.py
@@ -83,4 +83,4 @@ def process(filename):
 # Run the application
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port,debug=True)
